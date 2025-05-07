9d0560293ac8d146692f60aa9ac8d41fa74d497e
Roshan Singh <107341705+roshan9641@users.noreply.github.com>
2025-05-07 21:23:34 +0530

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
   } = useForm();
   const handleLogin = async (data) => {
     await axios
-      .post("http://localhost:3000/api/v1/user/login", data, {
+      .post("https://nxtwaveproject-backend.onrender.com/api/v1/user/login", data, {
         withCredentials: true,
         headers: {
           "Content-Type": "application/json",
