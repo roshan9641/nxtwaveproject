362268cf2a9061dfc3ebd405f78d544ea7974f7a
Roshan Singh <107341705+roshan9641@users.noreply.github.com>
2025-05-07 21:24:00 +0530

    Update Register.jsx

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -17,7 +17,7 @@ const Register = () => {
   const handleRegister = async (data) => {
     data.phone = `+91${data.phone}`;
     await axios
-      .post("http://localhost:3000/api/v1/user/register", data, {
+      .post("https://nxtwaveproject-backend.onrender.com/api/v1/user/register", data, {
         withCredentials: true,
         headers: { "Content-Type": "application/json" },
       })
